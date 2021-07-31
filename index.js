47a13ef51f02c0b77c123d6f2222f4e3799d75d0
ardianoldeen <ardianoleen@gmail.com>
2021-07-30 23:11:23 -0400

    fix bugs

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const path = require("path");
-
+const BlogPost = require("./models/BlogPost");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/mydatabase", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/mydatabase", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 const app = express();
 const ejs = require("ejs");
 app.set("view engine", "ejs");
 
+let ejsOptions = {
+  // delimiter: '?', Adding this to tell you do NOT use this like I've seen in other docs, does not work for Express 4
+  async: true,
+};
+
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const blogposts = await BlogPost.find({});
+  res.render("index", { blogposts });
   //res.sendFile(path.resolve(__dirname, "pages/index.html"));
 });
 app.get("/about", (req, res) => {
@@ -25,6 +37,16 @@ app.get("/contact", (req, res) => {
 app.get("/post", (req, res) => {
   // res.sendFile(pgitath.resolve(__dirname, "pages/post.html"));
   res.render("post");
+});
+
+app.get("/post/new", (req, res) => {
+  res.render("create");
+});
+
+app.post("/post/store", async (req, res) => {
+  await BlogPost.create(req.body, (error, blogpost) => {
+    res.redirect("/");
+  });
 });
 
 app.listen(3000, () => {
