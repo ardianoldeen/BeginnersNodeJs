29c59588857668291b5e27ffa67161f48e99aa30
ardianoldeen <ardianoleen@gmail.com>
2021-07-31 10:45:02 -0400

    refactor to MVC

---
+++ controllers/storePost.js
@@ -0,0 +1,9 @@
+const BlogPost = require("../models/BlogPost");
+module.exports = async (req, res) => {
+  let image = req.files.image;
+  image.mv(path.resolve(__dirname, "public/img", image.name), async (error) => {
+    console.log(error);
+    await BlogPost.create({ ...req.body, image: "/img/" + image.name });
+  });
+  res.redirect("/");
+};
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const path = require("path");
-const BlogPost = require("./models/BlogPost");
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/mydatabase", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
+const fileUpload = require("express-fileupload");
+const newPostController = require("./controllers/newPost");
+const createHomeController = require("./controllers/createHome");
+const getPostController = require("./controllers/getPost");
+const storePostController = require("./controllers/storePost");
 
 const app = express();
 const ejs = require("ejs");
@@ -15,40 +18,27 @@ let ejsOptions = {
   // delimiter: '?', Adding this to tell you do NOT use this like I've seen in other docs, does not work for Express 4
   async: true,
 };
+const validateMiddleWare = (req, res, next) => {
+  if (req.files == null || req.body.title == null) {
+    return res.redirect("/post/new");
+  }
+  next();
+};
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/", async (req, res) => {
-  const blogposts = await BlogPost.find({});
-  res.render("index", { blogposts });
-  //res.sendFile(path.resolve(__dirname, "pages/index.html"));
-});
-app.get("/about", (req, res) => {
-  // res.sendFile(path.resolve(__dirname, "pages/about.html"));
-  res.render("about");
-});
-app.get("/contact", (req, res) => {
-  // res.sendFile(path.resolve(__dirname, "pages/contact.html"));
-  res.render("contact");
-});
-app.get("/post", (req, res) => {
-  // res.sendFile(pgitath.resolve(__dirname, "pages/post.html"));
-  res.render("post");
-});
-
-app.get("/post/new", (req, res) => {
-  res.render("create");
-});
-
-app.post("/post/store", async (req, res) => {
-  await BlogPost.create(req.body, (error, blogpost) => {
-    res.redirect("/");
-  });
-});
+app.use(fileUpload());
+app.use("/post/store", validateMiddleWare);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
 });
+
+app.get("/", createHomeController);
+
+app.get("/post/new", newPostController);
+
+app.get("/post/:id", getPostController);
+
+app.post("/post/store", storePostController);
